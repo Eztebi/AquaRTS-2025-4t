93e49b14d654a1aad3d63cde70ae5d1f55acbece
Eztebi <derekderek2012@hotmail.com>
2025-11-04 21:15:41 -0600

    Move and Action of Aldeano

--- Source/AquaRTS/CharacterRTS.cpp
+++ Source/AquaRTS/CharacterRTS.cpp
@@ -8,8 +8,6 @@ ACharacterRTS::ACharacterRTS()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-
 }
 
 // Called when the game starts or when spawned
