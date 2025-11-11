86ba8bd30c208c29620ff8577d56c4b376f644c6
Eztebi <derekderek2012@hotmail.com>
2025-11-11 15:23:37 -0600

    FixedMovement

---
+++ Source/AquaRTS/Recolector.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterRTS.h"
+#include "Recolector.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AQUARTS_API ARecolector : public ACharacterRTS
+{
+	GENERATED_BODY()
+	
+};
